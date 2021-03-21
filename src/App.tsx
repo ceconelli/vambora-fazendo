3794a86e45a12dc04ae0a28678e494725d0bc113
Gustavo Ceconelli <guceconelli@hotmail.com>
2021-03-21 13:56:12 -0300

    add grid and show notes list on it

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Navbar from './parts/components/navbar/Navbar'
+import NotesGrid from './components/note/NotesGrid'
 
 function App(): any {
   return (
     <div className="App">
       <Navbar title="Vambora Fazendo - A simple to-do list application"/>
+      <NotesGrid />
     </div>
   );
 }
---
+++ src/components/note/NotesGrid.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Note from './Note'
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+      maxWidth: 752,
+    },
+    demo: {
+      backgroundColor: theme.palette.background.paper,
+    },
+    title: {
+      margin: theme.spacing(4, 0, 2),
+    },
+  }),
+);
+
+function generate(element: React.ReactElement) {
+  return [0, 1, 2].map((value) =>
+    React.cloneElement(element, {
+      key: value,
+    }),
+  );
+}
+
+
+
+const NotesGrid = () => {
+    const classes = useStyles();
+    const [dense, setDense] = React.useState(false);
+    const [secondary, setSecondary] = React.useState(false);
+
+    return (
+        <Grid item xs={12} md={12}>
+          <Typography variant="h6" className={classes.title}>
+            Your Notes
+          </Typography>
+          <div className={classes.demo}>
+            <List dense={dense}>
+              {generate(
+                <Note />,
+              )}
+            </List>
+          </div>
+        </Grid>
+    )
+}
+
+
+export default NotesGrid
